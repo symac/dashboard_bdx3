b7d5e42d86f8ce607c87fbb5218f5fb2f66351b0
symac <wikitipiac@laposte.net>
2013-11-25 10:33:40 +0100

    Suggestion pour scrapping

--- dashboard_bdx/webscraping.php
+++ dashboard_bdx/webscraping.php
@@ -1,24 +1,26 @@
 <?php
-$html = file_get_contents('http://babordplus.univ-bordeaux.fr/notice.php?q=pour%20les%20nuls&spec_expand=1&sort_de	fine=&sort_order=&rows=&start=1'); //get the html returned from the following url
-$babord_doc = new DOMDocument();
-//preg_replace_all('/\/\*[^]*?\*\//g', '', $html);
-libxml_use_internal_errors(TRUE); //disable libxml errors
-
-
-if(!empty($html)){ //if any html is actually returned
-
-  $babord_doc->loadHTML($html);
-  libxml_clear_errors(); //remove errors for yucky html
+  require_once("simple_html_dom.php");
+  $html = file_get_html('http://babordplus.univ-bordeaux.fr/notice.php?q=pour%20les%20nuls&spec_expand=1&sort_define=&sort_order=&rows=&start=1&ct=bx3_ws');
   
-  $babord_xpath = new DOMXPath($babord_doc);
-
-  $babord_row = $babord_xpath->query('//h5');
-	
-  if($babord_row->length > 0){
-      foreach($babord_row as $row){
-		echo $row->nodeValue;
-      }
+  $title = getZone("h1[class=book_title]");
+  $creator = getZone("dd[class=auteur_principal]");
+  
+  print($title)."\n";
+  print($creator)."\n";
+  
+  
+  
+  function getZone($search)
+  {
+    global $html;
+    if ($html->find($search))
+    {
+      return trim($html->find($search, 0)->plaintext);
+    }
+    else
+    {
+      return "";
+    }
   }
-}
-
+  
 ?>
